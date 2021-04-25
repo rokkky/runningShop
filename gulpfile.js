038582149fa34261e17154f1528b4f8f31cc76f5
rokkky <ka.egor1999@gmail.com>
2021-04-25 18:13:02 +0300

    complete promo, header

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function (done) {
-  gulp.src('./src/sass/**/styles.scss')
+  gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))  
     .pipe(gulp.dest('./src/css'));
     done();
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/styles.scss', gulp.parallel('sass'));
+  gulp.watch('./src/sass/**/*.scss', gulp.parallel('sass'));
 });
